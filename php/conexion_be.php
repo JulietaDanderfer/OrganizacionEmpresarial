316dc506b8a1b84a1aa62350edac7abf105061de
Julieta Danderfer <133677766+JulietaDanderfer@users.noreply.github.com>
2024-06-27 14:27:43 -0300

    Add files via upload

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+    $conexion = mysqli_connect("localhost","root","","login_register_db");
+
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la BD';
+    }else{
+        echo 'No se pudo conectar a la BD';
+    }
+    */
+    
+?>
